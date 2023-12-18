525e222b015acaf44e1d9b1ba6b058ac07b8eb8b
Yusuke Ashida <sadao@SADAOPC>
2023-12-19 03:08:03 +0900

    効率的な照合方法での検索機能を追加した

--- src/App.java
+++ src/App.java
@@ -1,11 +1,12 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        char[] text = {'A','B','C','D','E','F','G','H','I','J'};
-        char[] pattern = {'I', 'I', 'J'};
+        char[] text = {'H','I','P','O','P','O','T','A','M','U','S'}; // 元のテキスト
+        char[] pattern = {'A','M','U'}; // パターン
 
-        int result = search2(text, pattern);
-
-        System.out.println(result);
+        int result1 = search2(text, pattern);
+        System.out.println(result1 + "番目の位置で合致しました。");
+        int result2 = search3(text, pattern);
+        System.out.println(result2 + "番目の位置で合致しました。");
     }
 
     // 参考書の関数
@@ -26,17 +27,125 @@ public class App {
 
     // 参考書を元に自分で変更した関数
     public static int search2(char[] T, char[] P) {
+        int count = 0;
         for(int i=0; i<T.length-P.length+1; i++){
             for(int j=0; j<P.length; j++){
                 System.out.println(T[i+j] + "と" + P[j] + "の比較をする");
+                count++;
                 if(T[i+j] == P[j]){
-                    if(j == P.length-1)
+                    if(j == P.length-1){
+                        System.out.println("比較回数は " + count + "です。");
                         return i+1;
+                    }
                 }else{
                     break;
                 }
             }
         }
         return -1;  // パターンが見つからない場合は-1を返す
+    }
+
+    public static int search3(char[] T, char[] P){
+        int count = 0;
+        int length = checkLength(P);
+        char[] C = new char[length];
+        int[] D = new int[length];
+        for(int x=0; x<length; x++){
+            C[x] = ' ';
+            D[x] = P.length;
+        }
+
+        // System.out.println("C, Dテーブルの初期値");
+        // returnTable(C);
+        // returnTable(D);
+
+        // 票の作成
+        for(int j=0; j < P.length-1; j++){
+            for(int k=0; k<=j || k<length; k++){
+                if(C[k]==P[j] || C[k]==' '){
+                    C[k] = P[j];
+                    D[k] = P.length-j-1;
+                    break;
+                }
+            }
+        }
+
+        // System.out.println("表の作成後");
+        // returnTable(C);
+        // returnTable(D);
+
+        // 文字列を照合する
+        int i=0;
+        int d=0;
+        while(i<=T.length-P.length+1){
+            for(int j=0; j<P.length; j++){
+                System.out.println(T[i+j] + "と" + P[j] + "の比較をする");
+                count++;
+                if(T[i+j] == P[j]){
+                    if(j==P.length-1){
+                        System.out.println("比較回数は " + count + "です。");
+                        return i+1;
+                    }
+                }else{
+                    break;
+                }
+            }
+
+            // スキップ数の決定
+            for(int k=0; k<length; k++){
+                if(C[k] == T[i+P.length-1] || C[k] == ' '){
+                    d = D[k];
+                    break;
+                }
+            }
+
+            i = i+d;
+        }
+
+        System.out.println("比較回数は " + count + "です。");
+        return -1;
+    }
+
+    public static void returnTable(int[] array){
+        System.out.print("スキップ数 : ");
+        for(int i=0; i<array.length; i++){
+            if(i==0){
+                System.out.print(array[i]);
+                continue;
+            }
+            System.out.print(", " + array[i]);
+            if(i==array.length-1)
+                System.out.print("\n");
+        }
+    }
+
+    public static void returnTable(char[] array){
+        System.out.print("判定文字   : ");
+        for(int i=0; i<array.length; i++){
+            if(i==0){
+                System.out.print(array[i]);
+                continue;
+            }
+            System.out.print(", " + array[i]);
+            if(i==array.length-1)
+                System.out.print("\n");
+        }
+    }
+
+    public static int checkLength(char[] array){
+        int result = array.length;
+        int charCount = 0;
+        int overlap = 0;
+        for(int i=0; i<array.length; i++){
+            for(int j=0; j<array.length; j++){
+                if(array[i] == array[j])
+                    charCount++;
+            }
+            if(charCount > 1)
+                overlap++;
+
+                charCount = 0;
+        }
+        return result - overlap/2;
     }
 }
